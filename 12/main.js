150b650c7edcb4cca6363ce364b5ef535646e983
NeDonil <gatfild@bk.ru>
2020-07-27 01:21:06 +0300

    move bracket

--- 12/main.js
+++ 12/main.js
@@ -9,13 +9,14 @@ ball.on("click", function()
 {
     var xpos = (dir * 750).toString();
     var ypos = (Math.random() * (526 - 50) + 50).toString();
-    
+
     ball.animate({"left": "+=" + xpos,"top": ypos}, 
                 2000,
                 function()
                 {
                     if(parseInt(ball.css('top')) > 280 && parseInt(ball.css('top')) < 380)
                         alert("Goal!!!");
-                });
+                }
+    );
     dir *= -1;
 });
