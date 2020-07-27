d909c54a4b85a23884cc5dd58dfcb69a7fb6287c
AlsoNeDonil <svinoukhov03@gmail.com>
2020-07-27 19:33:27 +0300

    const correctness

--- 12/main.js
+++ 12/main.js
@@ -1,6 +1,6 @@
 //getting objects for logic
-var width = 800;
-var height = 526;
+const WIDTH = 800;
+const HEIGHT = 526;
 var ball = $(".ball");
 
 var dir = 1;
@@ -8,7 +8,7 @@ var dir = 1;
 ball.on("click", function()
 {
     var xpos = (dir * 750).toString();
-    var ypos = (Math.random() * (526 - 50) + 50).toString();
+    var ypos = (Math.random() * (HEIGHT - 50) + 50).toString();
 
     ball.animate({"left": "+=" + xpos,"top": ypos}, 
                 2000,
